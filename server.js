3dae526adca497df705f31900f904cac0e436fcb
santa4246 <santa4246@naver.com>
2022-10-15 17:32:47 +0900

    nodemon 설치, bootstrap 추가, /write 페이지 post 요청 api 구현

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 /* 복습파일 */
+
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true}));
 
-app.listen(8080, function(){
+app.listen(8080, function () {
   console.log('listening on 8080');
 });
 
-app.get('/test', function(req, res){
-  res.send('테스트 페이지 입니다.');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function(req, res){
+  res.sendFile(__dirname + '/write.html');
+})
+
+app.post('/add', function(req, res){
+  console.log(req.body);
+  res.send('전송완료');
 });
